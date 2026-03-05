b57da8498f7a8d24513a45fa327adcec92948cab
heidi-dang <heidi.dang.dev@gmail.com>
2026-03-06 00:41:20 +1100

    Revert "ui: add Aurora polish layer with gradient edges and glow effects"
    This reverts commit 77cca7efe8e3a247125bb225b8f713b08c0f7aeb.

--- packages/ui/src/components/dialog.tsx
+++ packages/ui/src/components/dialog.tsx
@@ -30,7 +30,6 @@ export function Dialog(props: DialogProps) {
           classList={{
             ...(props.classList ?? {}),
             [props.class ?? ""]: !!props.class,
-            "aurora-modal": true,
           }}
           onOpenAutoFocus={(e) => {
             const target = e.currentTarget as HTMLElement | null
